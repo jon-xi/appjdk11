950e57ddde83d04a86f4840f20e428a07a1ebd4d
Jon Xi <xiqiang0523@gmail.com>
2024-11-13 18:09:36 +0800

    Update Part02Mono.java

--- src/main/java/com/mycompany/app/literx/Part02Mono.java
+++ src/main/java/com/mycompany/app/literx/Part02Mono.java
@@ -1,4 +1,9 @@
-package com.mycompany.app.literx;
+package io.pivotal.literx;
+
+//generic imports to help with simpler IDEs (ie tech.io)
+import java.util.*;
+import java.util.function.*;
+import java.time.*;
 
 import reactor.core.publisher.Mono;
 
@@ -6,36 +11,36 @@ import reactor.core.publisher.Mono;
  * Learn how to create Mono instances.
  *
  * @author Sebastien Deleuze
- * @see <a href="http://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html">Mono Javadoc</a>
+ * @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html">Mono Javadoc</a>
  */
 public class Part02Mono {
 
 //========================================================================================
 
-    // TODO Return an empty Mono
-    Mono<String> emptyMono() {
-        return Mono.empty();
-    }
+	// TODO Return an empty Mono
+	Mono<String> emptyMono() {
+		return Mono.empty();
+	}
 
 //========================================================================================
 
-    // TODO Return a Mono that never emits any signal
-    Mono<String> monoWithNoSignal() {
-        return Mono.never();
-    }
+	// TODO Return a Mono that never emits any signal
+	Mono<String> monoWithNoSignal() {
+		return Mono.never();
+	}
 
 //========================================================================================
 
-    // TODO Return a Mono that contains a "foo" value
-    Mono<String> fooMono() {
-        return Mono.just("foo");
-    }
+	// TODO Return a Mono that contains a "foo" value
+	Mono<String> fooMono() {
+		return Mono.just("foo");
+	}
 
 //========================================================================================
 
-    // TODO Create a Mono that emits an IllegalStateException
-    Mono<String> errorMono() {
-        return Mono.error(new IllegalStateException());
-    }
+	// TODO Create a Mono that emits an IllegalStateException
+	Mono<String> errorMono() {
+		return Mono.error(new IllegalStateException());
+	}
 
 }
