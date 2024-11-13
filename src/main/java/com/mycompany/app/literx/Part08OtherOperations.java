addc1fa7209065e3a29f11cbeaa09a51220743f3
Jon Xi <xiqiang0523@gmail.com>
2024-11-13 18:22:52 +0800

    Update Part08OtherOperations.java

--- src/main/java/com/mycompany/app/literx/Part08OtherOperations.java
+++ src/main/java/com/mycompany/app/literx/Part08OtherOperations.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2002-2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.pivotal.literx;
 
 //generic imports to help with simpler IDEs (ie tech.io)
@@ -21,76 +5,66 @@ import java.util.*;
 import java.util.function.*;
 import java.time.*;
 
-import java.util.concurrent.CompletableFuture;
-
 import io.pivotal.literx.domain.User;
-import io.reactivex.rxjava3.core.Flowable;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Single;
-import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import io.reactivex.rxjava3.core.BackpressureStrategy;
+
+import java.util.List;
 
 /**
- * Learn how to adapt from/to RxJava 3 Observable/Single/Flowable and Java 8+ CompletableFuture.
- *
- * Mono and Flux already implements Reactive Streams interfaces so they are natively
- * Reactive Streams compliant + there are {@link Mono#from(Publisher)} and {@link Flux#from(Publisher)}
- * factory methods.
- *
- * For RxJava 3, you should not use Reactor Adapter but only RxJava 3 and Reactor Core.
+ * Learn how to use various other operators.
  *
  * @author Sebastien Deleuze
  */
-public class Part09Adapt {
+public class Part08OtherOperations {
 
 //========================================================================================
 
-	// TODO Adapt Flux to RxJava Flowable
-	Flowable<User> fromFluxToFlowable(Flux<User> flux) {
-		return Flowable.fromPublisher(flux);
-	}
-
-	// TODO Adapt RxJava Flowable to Flux
-	Flux<User> fromFlowableToFlux(Flowable<User> flowable) {
-		return Flux.from(flowable);
+	// TODO Create a Flux of user from Flux of username, firstname and lastname.
+	Flux<User> userFluxFromStringFlux(Flux<String> usernameFlux, Flux<String> firstnameFlux, Flux<String> lastnameFlux) {
+		return Flux.zip(usernameFlux, firstnameFlux, lastnameFlux).map(t -> new User(t.getT1(), t.getT2(), t.getT3()));
 	}
 
 //========================================================================================
 
-	// TODO Adapt Flux to RxJava Observable
-	Observable<User> fromFluxToObservable(Flux<User> flux) {
-        return Observable.fromPublisher(flux);
-	}
-
-	// TODO Adapt RxJava Observable to Flux
-	Flux<User> fromObservableToFlux(Observable<User> observable) {
-		return Flux.from(observable.toFlowable(BackpressureStrategy.BUFFER));
+	// TODO Return the mono which returns its value faster
+	Mono<User> useFastestMono(Mono<User> mono1, Mono<User> mono2) {
+		return Mono.firstWithValue(mono1, mono2);
 	}
 
 //========================================================================================
 
-	// TODO Adapt Mono to RxJava Single
-	Single<User> fromMonoToSingle(Mono<User> mono) {
-		return Single.fromPublisher(mono);
-	}
-
-	// TODO Adapt RxJava Single to Mono
-	Mono<User> fromSingleToMono(Single<User> single) {
-		return Mono.from(single.toFlowable());
+	// TODO Return the flux which returns the first value faster
+	Flux<User> useFastestFlux(Flux<User> flux1, Flux<User> flux2) {
+		return Flux.firstWithValue(flux1, flux2);
 	}
 
 //========================================================================================
 
-	// TODO Adapt Mono to Java 8+ CompletableFuture
-	CompletableFuture<User> fromMonoToCompletableFuture(Mono<User> mono) {
-		return mono.toFuture();
+	// TODO Convert the input Flux<User> to a Mono<Void> that represents the complete signal of the flux
+	Mono<Void> fluxCompletion(Flux<User> flux) {
+        return flux.then(); // TO BE REMOVED
 	}
 
-	// TODO Adapt Java 8+ CompletableFuture to Mono
-	Mono<User> fromCompletableFutureToMono(CompletableFuture<User> future) {
-		return Mono.fromFuture(future);
+//========================================================================================
+
+	// TODO Return a valid Mono of user for null input and non null input user (hint: Reactive Streams do not accept null values)
+	Mono<User> nullAwareUserToMono(User user) {
+		return Mono.justOrEmpty(user);
+	}
+
+//========================================================================================
+
+	// TODO Return the same mono passed as input parameter, expect that it will emit User.SKYLER when empty
+	Mono<User> emptyToSkyler(Mono<User> mono) {
+		return mono.defaultIfEmpty(User.SKYLER);
+	}
+
+//========================================================================================
+
+	// TODO Convert the input Flux<User> to a Mono<List<User>> containing list of collected flux values
+	Mono<List<User>> fluxCollection(Flux<User> flux) {
+		return flux.collectList();
 	}
 
 }
